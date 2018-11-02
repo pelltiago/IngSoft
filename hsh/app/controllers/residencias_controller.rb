347b198fdc854d985ae87e88778507c41bf02f82
chilli-willi <gali7.jag@gmail.com>
2018-11-02 18:50:39 -0300

    cree residencias

---
+++ hsh/app/controllers/residencias_controller.rb
@@ -0,0 +1,2 @@
+class ResidenciasController < ApplicationController
+end
---
+++ hsh/test/controllers/residencias_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ResidenciasControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
