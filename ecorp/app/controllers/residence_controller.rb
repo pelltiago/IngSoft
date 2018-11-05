05101556d0ef5799161191431ba7e54571b40393
Santi <pellegrino.santi@gmail.com>
2018-11-04 22:18:10 -0300

    new create

--- ecorp/app/controllers/residence_controller.rb
+++ ecorp/app/controllers/residence_controller.rb
@@ -1,8 +1,11 @@
 class ResidenceController < ApplicationController
   def new
+  	@residence = Residence.new
   end
 
   def create
+  	@residence = Residence.new(params.require(:residence).permit(:name, :desc, :location))
+  	redirect_to residence_path, notice: "Residencia creada con exito"
   end
 
   def destroy
