9a5bbee1c35f740bcc9d5cf47cfad7122a8e99fe
chilli-willi <gali7.jag@gmail.com>
2018-11-04 23:56:22 -0300

    ultima del dom

--- ecorp/app/controllers/residence_controller.rb
+++ ecorp/app/controllers/residence_controller.rb
@@ -1,10 +1,13 @@
 class ResidenceController < ApplicationController
+ 
+
   def new
   	@residences = Residence.new
   end
 
   def create
-  	@residences = Residence.new
+  	#@residences = Residence.new(params.require(:residence).permit(:name, :desc, :location))
+  	#redirect_to residence_path, notice: "Residencia creada con exito"
   end
 
   def show
--- ecorp/config/routes.rb
+++ ecorp/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
   resources :residence
+  get 'residence/create', to: 'residences#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
