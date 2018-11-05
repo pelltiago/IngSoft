8e225be6a865a85603a8c6faf7a1d7eda4e18137
chilli-willi <gali7.jag@gmail.com>
2018-11-04 22:10:16 -0300

    comit

--- ecorp/config/routes.rb
+++ ecorp/config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'residence/new'
-  get 'residence/create'
-  get 'residence/destroy'
+
+  resources :residence
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
